fb92d5296cefd3c1be581a2a2cf1ce4c76686e3e
Saptarshi Paul <saptarshi2027paul@gmail.com>
2024-10-24 13:00:43 +0530

    Changed the Routing Paths

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Hero from "./Hero";
 import Workflow from "./workflow";
 import Testimonials from "./Testanomials";
+import Navbar from "./navbar";
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <>
+      <Navbar/>
       <Hero />
       <Workflow />
       <Testimonials />
+      <Footer/>
     </>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,44 +3,7 @@ import React from "react";
 
 const Login = () => {
   return (
-    <main className="h-[52vh] w-full">
-      <div className="">
-        <div className=" w-fit p-5 text-center text-white border-2 border-white rounded-md absolute top-1/3 left-1/2 -translate-x-[50%] -translate-y-[50%]">
-          <h1 className="text-2xl my-3">Login</h1>
-          <form className="space-y-4" action="">
-            
-            <div className="input grid grid-cols-3">
-              <label htmlFor="">Email : </label>
-              <input
-                className="col-span-2 bg-black outline-none px-2 py-1"
-                type="email"
-                placeholder="ck@gmail.com"
-              />
-            </div>
-            <div className="input grid grid-cols-3">
-              <label htmlFor="">Password: </label>
-              <input
-                className="col-span-2 bg-black outline-none px-2 py-1"
-                type="password"
-                placeholder="********"
-              />
-            </div>
-            <button className="border-2 border-gray-50 rounded-md  px-2 py-1">
-              Login
-            </button>
-          </form>
-          <div className="text-center my-4">
-            <p>--------or------</p>
-            <h1>
-              Don't have an Account{" "}
-              <a className="text-blue-600" href="/signup">
-                SingUp
-              </a>
-            </h1>
-          </div>
-        </div>
-      </div>
-    </main>
+      <h1>Login</h1>
   );
 };
 
